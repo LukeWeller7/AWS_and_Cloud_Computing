67d6fb318caa1a07997ab0f0691a7c08a0781edf
Luke Weller <lp.weller01@gmail.com>
2023-10-03 15:56:58 +0100

    Notes on Mongodb

---
+++ aws/app_deployment/mongodb/mongodb_provisional.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt upgrade -y
+
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
+
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
+sudo apt update
+
+sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
